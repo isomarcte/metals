80a2e0fbc87f017966c3033376c90b48a8d14c5b
David Strawn <isomarcte@gmail.com>
2019-12-06 16:57:59 -0700

    squash!

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 function bloop_version {
   grep "val bloop" build.sbt | sed 's|[^0-9.]||g'
 }
@@ -7,5 +9,7 @@ function bloop_version {
 curl -Lo coursier https://git.io/coursier-cli && chmod +x coursier
 ./coursier launch ch.epfl.scala:bloopgun-core_2.12:$(bloop_version) -- about
 
-sbt "$1"
+sed -i 's/\r//g' .jvmopts
+
+bash -x $(which sbt) "$1"
 # sbt must be the last command - its exit code signals if tests passed or not
